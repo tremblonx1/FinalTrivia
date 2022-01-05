bc7abba4b43d2d4e5b213eebd25967010a954f09
tremblonx1 <92490575+tremblonx1@users.noreply.github.com>
2022-01-05 23:16:54 +0000

    update

--- finalttrivia/src/utilities/db.js
+++ finalttrivia/src/utilities/db.js
@@ -1,18 +1,1 @@
-//import { app } from '../utilities/init-firebase'
-
-//export const addQuiz = async (quizData) => {
-  //let response = app.firestore().collection('quiz').add(quizData);
- // return response;
-//};
-
-///export const getAllQuiz = async () => {
- // const snapshot = app.firestore().collection('quiz').get();
- // const quiz = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
- // return quiz;
-//};
-
-//export const getAllUsers = async () => {
-//  const snapshot = app.firestore().collection('users').get();
-//  const users = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-//  return users;
-//}
+import {db} from '../utilities/init-firebase'
